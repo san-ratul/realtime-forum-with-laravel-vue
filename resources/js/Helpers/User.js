156691db1119fa412f4754268c54c01e086c978d
san_ratul <san.ratul001@gmail.com>
2020-08-03 17:56:23 +0600

    Helper Classes and Functions

---
+++ resources/js/Helpers/User.js
@@ -0,0 +1,50 @@
+import Token from "./Token";
+import AppStorage from "./AppStorage";
+
+class User {
+    login(data) {
+        axios.post('/api/auth/login', data)
+            .then(res => this.responseAfterLogin(res))
+            .catch(error => console.log(error.response.data))
+    }
+
+    responseAfterLogin(res) {
+        const access_token = res.data.access_token;
+        const user_name = res.data.user;
+        if (Token.isValid(access_token)) {
+            AppStorage.store(user_name, access_token);
+        }
+    }
+
+    hasToken() {
+        const storedToken = AppStorage.getToken();
+        if (storedToken) {
+            return Token.isValid(storedToken) ? true : false;
+        }
+
+        return false;
+    }
+
+    loggedin() {
+        return this.hasToken();
+    }
+
+    logout() {
+        AppStorage.clear();
+    }
+
+    name() {
+        if (this.loggedin()) {
+            return AppStorage.getUser();
+        }
+    }
+
+    id() {
+        if (this.loggedin()) {
+            const payload = Token.payload(AppStorage.getToken());
+            return payload.sub;
+        }
+    }
+}
+
+export default User = new User();
